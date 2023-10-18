719cadf50e69073f4848fb3fc8119f5b1463d695
sample <sample@naver.com>
2023-10-18 15:55:49 +0900

    Feat : 업로드 방식으로 수정

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -5,9 +6,13 @@ import nltk.data
 import heapq
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 nltk.download('punkt')
 nltk.download('stopwords')
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
 def index():
@@ -16,35 +21,40 @@ def index():
 @app.route('/summarize', methods=['POST'])
 def summarize_text():
     if request.method == 'POST':
-        text = request.form['text']
+        uploaded_file = request.files['file']
         num_sentences = int(request.form['num_sentences'])
 
-        sentences = sent_tokenize(text)
-        words = word_tokenize(text)
-        stop_words = set(stopwords.words('english'))
+        if uploaded_file:
+            text = uploaded_file.read().decode('utf-8')
 
-        words = [word for word in words if word.lower() not in stop_words]
+            sentences = sent_tokenize(text)
+            words = word_tokenize(text)
+            stop_words = set(stopwords.words('english'))
 
-        word_freq = {}
-        for word in words:
-            if word in word_freq:
-                word_freq[word] += 1
-            else:
-                word_freq[word] = 1
+            words = [word for word in words if word.lower() not in stop_words]
 
-        sentence_scores = {}
-        for sentence in sentences:
-            for word in word_tokenize(sentence.lower()):
+            word_freq = {}
+            for word in words:
                 if word in word_freq:
-                    if sentence not in sentence_scores:
-                        sentence_scores[sentence] = word_freq[word]
-                    else:
-                        sentence_scores[sentence] += word_freq[word]
+                    word_freq[word] += 1
+                else:
+                    word_freq[word] = 1
 
-        summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
-        summary = ' '.join(summary_sentences)
+            sentence_scores = {}
+            for sentence in sentences:
+                for word in word_tokenize(sentence.lower()):
+                    if word in word_freq:
+                        if sentence not in sentence_scores:
+                            sentence_scores[sentence] = word_freq[word]
+                        else:
+                            sentence_scores[sentence] += word_freq[word]
 
-        return render_template('summary.html', text=text, summary=summary)
+            summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
+            summary = ' '.join(summary_sentences)
+
+            return render_template('summary.html', summary=summary)
+
+    return "No file uploaded."
 
 if __name__ == '__main__':
     app.run(debug=True)
